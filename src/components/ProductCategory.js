23ac6e35631685a3f585472e4166655975cb5f6a
Jono Prest <jjprest@gmail.com>
2020-09-17 09:39:18 +0200

    shop page pulls data from sheet

--- src/components/ProductCategory.js
+++ src/components/ProductCategory.js
@@ -9,73 +9,41 @@ import {
   Button,
 } from "reactstrap";
 
-const ProductCategory = () => {
+const noImage = require("assets/img/no-image.jpg")
+
+const ProductCategory = ({ title, products }) => {
   return (
+    <>
+    <h2>{title}</h2>
     <div className="d-flex overflow-auto" style={{ width: "800px" }}>
-      <Card className="mx-3" style={{ width: "20rem", minWidth: "20rem" }}>
-        <CardImg
-          top
-          src="https://images.unsplash.com/photo-1518635017498-87f514b751ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80"
-          alt="..."
-        />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Button color="primary">Go somewhere</Button>
-        </CardBody>
-      </Card>
-
-      <Card className="mx-3" style={{ width: "20rem", minWidth: "20rem"  }}>
-        <CardImg
-          top
-          src="https://images.unsplash.com/photo-1509912760195-4f6cfd8cce2c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-          alt="..."
-        />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Button color="primary">Go somewhere</Button>
-        </CardBody>
-      </Card>
-
-      <Card className="mx-3" style={{ width: "20rem", minWidth: "20rem"  }}>
-        <CardImg
-          top
-          src="https://images.unsplash.com/photo-1481900369621-54a7facacc6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1955&q=80"
-          alt="..."
-        />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Button color="primary">Go somewhere</Button>
-        </CardBody>
-      </Card>
-
-      <Card className="mx-3" style={{ width: "20rem", minWidth: "20rem"  }}>
-        <CardImg
-          top
-          src="https://images.unsplash.com/photo-1486297678162-eb2a19b0a32d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80"
-          alt="..."
-        />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardText>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </CardText>
-          <Button color="primary">Go somewhere</Button>
-        </CardBody>
-      </Card>
+      
+      {products.map((product) => {
+        return (
+          <Card className="mx-3" style={{ width: "15rem", minWidth: "15rem" }}>
+            <CardImg
+              top
+              src={product.Image !== "" ? product.Image : noImage}
+              alt="..."
+            />
+            <CardBody>
+              <CardTitle>{product.Item}</CardTitle>
+              <CardText>
+                Supplier: {product.Supplier}
+                <br/>
+                Container: {product.Container}
+                <br/>
+                Quantity: {product.Quantity}{product.Unitss}
+                <br/>
+                Price: {product.Price}
+                <br/>
+              </CardText>
+              <Button color="primary">Go somewhere</Button>
+            </CardBody>
+          </Card>
+        );
+      })}
     </div>
+    </>
   );
 };
 
--- src/views/ShopPage/ShopPage.js
+++ src/views/ShopPage/ShopPage.js
@@ -1,29 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Papa from "papaparse";
 
 import "./ShopPage.css";
 
 // core components
 import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
-import NucleoIcons from "views/NucleoIcons.js";
 import DemoFooter from "components/Footers/DemoFooter.js";
 import ProductCategory from "components/ProductCategory";
-import ProfilePageHeader from "components/Headers/ProfilePageHeader";
+import ShopHeader from "components/Headers/ShopHeader";
+
+import { database } from "components/database";
 
 const ShopPage = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const proxyUrl = "https://agile-anchorage-79298.herokuapp.com/";
+    const apiURL =
+      "https://docs.google.com/spreadsheets/d/1T2EV-ArYBTgchH1h89pqK0ffc77EDTffItpNqoHosd0/export?format=csv";
+
+    //Get google sheet data using papa parse
+
+    Papa.parse(proxyUrl + apiURL, {
+      download: true,
+      header: true,
+      skipEmptyLines: true,
+      comments: "#",
+      complete: function (results) {
+        setData(results.data);
+      },
+    });
+  }, []);
+
+  const uniqueCategoryArray = data.filter((value, index, self) => {
+    return (
+      self.findIndex((v) => v.Category === value.Category) === index &&
+      value.Category !== "" &&
+      value.Category !== "Title"
+    );
+  });
+
   return (
-    <div
-      style={{
-        background: "black",
-      }}
-    >
+    <div>
       <ExamplesNavbar />
-      <ProfilePageHeader />
+      <ShopHeader />
 
       <div className="w-100 d-flex flex-column  align-items-center belowNav">
-        <ProductCategory />
-        <ProductCategory />
-        <ProductCategory />
-        <ProductCategory />
+        {uniqueCategoryArray.map((uniqueCategory) => {
+          const products = data.filter((value) => {
+            return value.Category === uniqueCategory.Category;
+          });
+          console.log("Products", products)
+          return <ProductCategory title={uniqueCategory.Category} products={products}/>;
+        })}
+        {/* <ProductCategory category={database.berries} data={data}/>
+        <ProductCategory category={database.eggs} />
+        <ProductCategory category={database.coffee} />
+        <ProductCategory category={database.dairy} /> */}
       </div>
 
       <DemoFooter />
