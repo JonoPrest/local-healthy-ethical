4b4f2ff2fbdfb29df684d7547048d71407ba9941
Jono Prest <jjprest@gmail.com>
2021-04-30 13:25:59 +0200

    added personal name and brought back button for quantity in mastersheet

--- src/components/MasterTable.js
+++ src/components/MasterTable.js
@@ -2,183 +2,237 @@ import React, { useEffect, useRef, useState } from "react";
 import { useReactToPrint } from "react-to-print";
 import { Col, Spinner, Table } from "reactstrap";
 import { mailToSuppliers } from "utils/mailUtils";
+import LongScrollingModal from "./LongScrollingModal";
 
 import "./MasterTable.css";
 
 const MasterTable = ({ monthOrdersArray, isLoading }) => {
-	const [masterObject, setMasterObject] = useState({});
-	const [masterArray, setMasterArray] = useState([]);
-	const [suppliersArray, setSuppliersArray] = useState([]);
-	const [total, setTotal] = useState("");
+  const [masterObject, setMasterObject] = useState({});
+  const [masterArray, setMasterArray] = useState([]);
+  const [suppliersArray, setSuppliersArray] = useState([]);
+  const [total, setTotal] = useState("");
 
-	useEffect(() => {
-		//Print settings
-		const style = document.createElement("style");
-		style.innerHTML = `@page {size: landscape}`;
-		style.id = "page-orientation";
-		document.head.appendChild(style);
+  useEffect(() => {
+    //Print settings
+    const style = document.createElement("style");
+    style.innerHTML = `@page {size: landscape}`;
+    style.id = "page-orientation";
+    document.head.appendChild(style);
 
-		//Creating the master object
-		let masterObj = {};
-		monthOrdersArray.forEach((order) => {
-			order.cart.forEach((cartItem) => {
-				let quantity = cartItem.quantity;
-				if (masterObj[cartItem.item.Code]) {
-					quantity = masterObj[cartItem.item.Code].quantity + cartItem.quantity;
-				}
-				masterObj[cartItem.item.Code] = {
-					item: cartItem.item,
-					quantity: quantity,
-				};
-			});
-		});
-		setMasterObject(masterObj);
-		let cumulativeSum = 0;
-		Object.values(masterObj).forEach((value, i) => {
-			cumulativeSum += Number(value.item.Price) * value.quantity;
-		});
-		setTotal(`R${cumulativeSum.toFixed(2)}`);
-	}, [monthOrdersArray]);
+    //Creating the master object
+    let masterObj = {};
 
-	//passes the master object into an array that can be reorderd
-	useEffect(() => {
-		const initialArray = Object.values(masterObject).map((value) => value);
-		setMasterArray(initialArray);
-	}, [masterObject]);
+    monthOrdersArray.forEach((order) => {
+      order.cart.forEach((cartItem) => {
+        let quantity = cartItem.quantity;
+        let previousUserInfo = {};
+        if (masterObj[cartItem.item.Code]) {
+          quantity =
+            masterObj[cartItem.item.Code].itemInfo.quantity + cartItem.quantity;
+          if (masterObj[cartItem.item.Code].userInfo) {
+            previousUserInfo = masterObj[cartItem.item.Code].userInfo;
+          }
+        }
 
-	//sets up the list of suppliers
-	useEffect(() => {
-		const filteredSuppliersArray = masterArray.filter((value, index, self) => {
-			return (
-				self.findIndex((v) => v.item.Supplier === value.item.Supplier) === index
-			);
-		});
+        masterObj[cartItem.item.Code] = {
+          itemInfo: {
+            item: cartItem.item,
+            quantity: quantity,
+          },
+          userInfo: {
+            ...previousUserInfo,
+            [order.user.id]: {
+              user: order.user.displayName,
+              quantity: cartItem.quantity,
+            },
+          },
+        };
+      });
+    });
+    console.log(masterObj);
+    setMasterObject(masterObj);
+    let cumulativeSum = 0;
+    Object.values(masterObj).forEach((value, i) => {
+      cumulativeSum +=
+        Number(value.itemInfo.item.Price) * value.itemInfo.quantity;
+    });
+    setTotal(`R${cumulativeSum.toFixed(2)}`);
+  }, [monthOrdersArray]);
 
-		const newSuppliersArray = filteredSuppliersArray.map((item) => ({
-			name: item.item.Supplier,
-			email: item.item.SupplierEmail,
-		}));
-		setSuppliersArray(newSuppliersArray);
-	}, [masterArray]);
+  //passes the master object into an array that can be reorderd
+  useEffect(() => {
+    const initialArray = Object.values(masterObject).map((value) => value);
+    setMasterArray(initialArray);
+  }, [masterObject]);
 
-	const reorderArray = (e) => {
-		const { innerText } = e.target;
-		const editedArray = [...masterArray].sort((a, b) =>
-			a.item[innerText].localeCompare(b.item[innerText])
-		);
+  //sets up the list of suppliers
+  useEffect(() => {
+    const filteredSuppliersArray = masterArray.filter((value, index, self) => {
+      return (
+        self.findIndex(
+          (v) => v.itemInfo.item.Supplier === value.itemInfo.item.Supplier
+        ) === index
+      );
+    });
 
-		setMasterArray(editedArray);
-	};
+    const newSuppliersArray = filteredSuppliersArray.map((item) => ({
+      name: item.itemInfo.item.Supplier,
+      email: item.itemInfo.item.SupplierEmail,
+      personalName: item.itemInfo.item.supplierInfo
+        ? item.itemInfo.item.supplierInfo["Supplier Name"]
+        : item.itemInfo.item.Supplier,
+    }));
+    setSuppliersArray(newSuppliersArray);
+  }, [masterArray]);
 
-	const componentRef = useRef();
-	const handlePrint = useReactToPrint({
-		content: () => componentRef.current,
-	});
+  const reorderArray = (e) => {
+    const { innerText } = e.target;
+    const editedArray = [...masterArray].sort((a, b) =>
+      a.itemInfo.item[innerText].localeCompare(b.itemInfo.item[innerText])
+    );
 
-	return (
-		<div className="my-4 mx-0 px-0">
-			{isLoading ? (
-				<Spinner />
-			) : (
-				<div
-					className="d-flex flex-wrap justify-content-around w-100"
-					styl={{ maxWidth: "1200px" }}
-				>
-					<Col xl="8" lg="12" md="12" sm="12" xs="12">
-						<div ref={componentRef} className="my-5 masterTable">
-							<Table>
-								<thead>
-									<tr>
-										<th onClick={reorderArray}>Category</th>
-										<th onClick={reorderArray}>Code</th>
-										<th onClick={reorderArray}>Item</th>
-										<th onClick={reorderArray}>Supplier</th>
-										<th onClick={reorderArray}>Container</th>
-										<th onClick={reorderArray}>Quantity</th>
-										<th onClick={reorderArray}>Units</th>
-										<th onClick={reorderArray}>Price</th>
-										<th>Units to Order</th>
-										<th>Total</th>
-									</tr>
-								</thead>
-								<tbody>
-									{masterArray.map((value) => {
-										return (
-											<tr key={value.item.Code}>
-												<th className="text-left">{value.item.Category}</th>
-												<th className="text-left">{value.item.Code}</th>
-												<th className="text-left" style={{ maxWidth: "200px" }}>
-													{value.item.Item}
-												</th>
-												<th className="text-left">{value.item.Supplier}</th>
-												<th className="text-left">{value.item.Container}</th>
-												<th className="text-right">{value.item.Quantity}</th>
-												<th className="text-left">{value.item.Units}</th>
-												<th className="text-right">{value.item.Price}</th>
-												<th className="text-right">{value.quantity}</th>
-												<th className="text-right">
-													R
-													{(value.quantity * Number(value.item.Price)).toFixed(
-														2
-													)}
-												</th>
-											</tr>
-										);
-									})}
-									<tr>
-										<th></th>
-										<th></th>
-										<th></th>
-										<th></th>
-										<th></th>
-										<th></th>
-										<th></th>
-										<th></th>
-										<th className="text-right">Total:</th>
-										<th className="text-right">{total}</th>
-									</tr>
-								</tbody>
-							</Table>
-						</div>
-					</Col>
-					<Col
-						xl="3"
-						lg="3"
-						md="4"
-						sm="8"
-						xs="12"
-						className="d-flex flex-column  p-4 border-bottom text-left"
-					>
-						<h2>Email Supplier:</h2>
-						<div
-							className="d-flex flex-column"
-							style={{ maxHeight: "500px", overflowY: "auto" }}
-						>
-							{suppliersArray.map((supplier) => (
-								<a
-									className="card p-2"
-									href={mailToSuppliers(masterArray, supplier)}
-								>
-									<h5 style={{ margin: "0" }}>
-										{supplier.name} - {supplier.email}
-									</h5>
-								</a>
-							))}
-						</div>
-					</Col>
-					<Col xl="12">
-						<button
-							onClick={handlePrint}
-							className="btn btn-primary m-1"
-							id="invoice-print"
-						>
-							<i className="fa fa-print"></i> Print Master Table
-						</button>
-					</Col>
-				</div>
-			)}
-		</div>
-	);
+    setMasterArray(editedArray);
+  };
+
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
+  return (
+    <div className="my-4 mx-0 px-0">
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div
+          className="d-flex flex-wrap justify-content-around w-100"
+          styl={{ maxWidth: "1200px" }}
+        >
+          <Col xl="8" lg="12" md="12" sm="12" xs="12">
+            <div ref={componentRef} className="my-5 masterTable">
+              <Table>
+                <thead>
+                  <tr>
+                    <th onClick={reorderArray}>Category</th>
+                    <th onClick={reorderArray}>Code</th>
+                    <th onClick={reorderArray}>Item</th>
+                    <th onClick={reorderArray}>Supplier</th>
+                    <th onClick={reorderArray}>Container</th>
+                    <th onClick={reorderArray}>Quantity</th>
+                    <th onClick={reorderArray}>Units</th>
+                    <th onClick={reorderArray}>Price</th>
+                    <th>Units to Order</th>
+                    <th>Total</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {masterArray.map((value) => {
+                    return (
+                      <tr key={value.itemInfo.item.Code}>
+                        <th className="text-left">
+                          {value.itemInfo.item.Category}
+                        </th>
+                        <th className="text-left">
+                          {value.itemInfo.item.Code}
+                        </th>
+                        <th className="text-left" style={{ maxWidth: "200px" }}>
+                          {value.itemInfo.item.Item}
+                        </th>
+                        <th className="text-left">
+                          {value.itemInfo.item.Supplier}
+                        </th>
+                        <th className="text-left">
+                          {value.itemInfo.item.Container}
+                        </th>
+                        <th className="text-right">
+                          {value.itemInfo.item.Quantity}
+                        </th>
+                        <th className="text-left">
+                          {value.itemInfo.item.Units}
+                        </th>
+                        <th className="text-right">
+                          {value.itemInfo.item.Price}
+                        </th>
+                        <th className="text-right">
+                          <LongScrollingModal
+                            buttonText={value.itemInfo.quantity}
+                            title={value.itemInfo.item.Item}
+                          >
+                            {Object.values(value.userInfo).map((userValue) => {
+                              return (
+                                <div>
+                                  <p>
+                                    {userValue.quantity}x {userValue.user}{" "}
+                                  </p>
+                                </div>
+                              );
+                            })}
+                          </LongScrollingModal>
+                        </th>
+                        <th className="text-right">
+                          R
+                          {(
+                            value.itemInfo.quantity *
+                            Number(value.itemInfo.item.Price)
+                          ).toFixed(2)}
+                        </th>
+                      </tr>
+                    );
+                  })}
+                  <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th className="text-right">Total:</th>
+                    <th className="text-right">{total}</th>
+                  </tr>
+                </tbody>
+              </Table>
+            </div>
+          </Col>
+          <Col
+            xl="3"
+            lg="3"
+            md="4"
+            sm="8"
+            xs="12"
+            className="d-flex flex-column p-4 border-bottom text-left"
+          >
+            <h2>Email Supplier:</h2>
+            <div
+              className="d-flex flex-column"
+              style={{ maxHeight: "500px", overflowY: "auto" }}
+            >
+              {suppliersArray.map((supplier) => (
+                <a
+                  className="card p-2"
+                  href={mailToSuppliers(masterArray, supplier)}
+                >
+                  <h5 style={{ margin: "0" }}>
+                    {supplier.name} - {supplier.email}
+                  </h5>
+                </a>
+              ))}
+            </div>
+          </Col>
+          <Col xl="12">
+            <button
+              onClick={handlePrint}
+              className="btn btn-primary m-1"
+              id="invoice-print"
+            >
+              <i className="fa fa-print"></i> Print Master Table
+            </button>
+          </Col>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default MasterTable;
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,128 +6,127 @@ import { Card, CardBody, CardText, Button } from "reactstrap";
 import LongScrollingModal from "./LongScrollingModal";
 
 const ProductCard = ({ product, addItem, supplierInfo }) => {
-	const [foundSupplierInfo, setFoundSupplierInfo] = useState({});
-	const [quantity, setQuantity] = useState(1);
-	const [btnText, setBtnText] = useState("Add to Cart");
-	const [btnColor, setBtnColor] = useState("primary");
-	const [btnDisabled, setBtnDisabled] = useState(false);
+  const [foundSupplierInfo, setFoundSupplierInfo] = useState({});
+  const [quantity, setQuantity] = useState(1);
+  const [btnText, setBtnText] = useState("Add to Cart");
+  const [btnColor, setBtnColor] = useState("primary");
+  const [btnDisabled, setBtnDisabled] = useState(false);
 
-	useEffect(() => {
-		const foundFromArray = supplierInfo.find(
-			(supplier) => supplier.Supplier === product.Supplier
-		);
-		if (foundFromArray) {
-			setFoundSupplierInfo(foundFromArray);
-		}
-	}, [product, supplierInfo]);
+  useEffect(() => {
+    const foundFromArray = supplierInfo.find(
+      (supplier) => supplier.Supplier === product.Supplier
+    );
+    if (foundFromArray) {
+      setFoundSupplierInfo(foundFromArray);
+    }
+  }, [product, supplierInfo]);
 
-	const addToCartClicked = (e) => {
-		e.preventDefault();
-		addItem(product, quantity);
-		setBtnText("Added");
-		setBtnColor("success");
-		setBtnDisabled(true);
-		setTimeout(() => {
-			setBtnText("Add to Cart");
-			setBtnColor("primary");
-			setBtnDisabled(false);
-			setQuantity(1);
-		}, 1500);
-	};
+  const addToCartClicked = (e) => {
+    e.preventDefault();
+    addItem({ ...product, supplierInfo: foundSupplierInfo }, quantity);
+    setBtnText("Added");
+    setBtnColor("success");
+    setBtnDisabled(true);
+    setTimeout(() => {
+      setBtnText("Add to Cart");
+      setBtnColor("primary");
+      setBtnDisabled(false);
+      setQuantity(1);
+    }, 1500);
+  };
 
-	return (
-		<Card
-			className="mx-3 card-product"
-			style={{ width: "20rem", minWidth: "20rem" }}
-		>
-			
-			<CardBody>
-				<h4 style={{ marginTop: "0", paddingTop: "0" }}>{product.Item}</h4>
-				<CardText>
-					<LongScrollingModal
-						buttonClassName="btn-link btn-primary ml-0 pl-0"
-						buttonText={`Supplier: ${product.Supplier}`}
-						title={product.Supplier}
-					>
-						{foundSupplierInfo.Description ? (
-							<p>{foundSupplierInfo.Description}</p>
-						) : (
-							<p>Sorry no info about the above supplier!</p>
-						)}
+  return (
+    <Card
+      className="mx-3 card-product"
+      style={{ width: "20rem", minWidth: "20rem" }}
+    >
+      <CardBody>
+        <h4 style={{ marginTop: "0", paddingTop: "0" }}>{product.Item}</h4>
+        <CardText>
+          <LongScrollingModal
+            buttonClassName="btn-link btn-primary ml-0 pl-0"
+            buttonText={`Supplier: ${product.Supplier}`}
+            title={product.Supplier}
+          >
+            {foundSupplierInfo.Description ? (
+              <p>{foundSupplierInfo.Description}</p>
+            ) : (
+              <p>Sorry no info about the above supplier!</p>
+            )}
 
-						<br />
-						{foundSupplierInfo.Website && (
-							<div>
-								<p>For more info visit their website:</p>
-								<a
-									href={`https://${foundSupplierInfo.Website}`}
-									target="_blank"
-								>
-									{foundSupplierInfo.Website}
-								</a>
-							</div>
-						)}
-					</LongScrollingModal>
-					<br />
-					<strong>Container:</strong> {product.Container}
-					<br />
-					<strong>Quantity:</strong> {product.Quantity}
-					{product.Units}
-					<br />
-					<strong>Price:</strong> R{product.Price}
-					<br />
-				</CardText>
-				<form
-					className="d-flex justify-content-around"
-					onSubmit={addToCartClicked}
-				>
-					<Button color={btnColor} type="submit" disabled={btnDisabled}>
-						{btnText}
-						{btnText === "Added" && (
-							<i
-								className="nc-icon nc-check-2 ml-4"
-								style={{ fontSize: "15px" }}
-							/>
-						)}
-					</Button>
-					<div className="d-flex justify-content-center">
-						<input
-							type="text"
-							value={quantity}
-							style={{ width: "30px" }}
-							className="m-3"
-							onChange={(e) => {
-								if (!Number(e.target.value) && e.target.value !== "") return;
-								setQuantity(Number(e.target.value));
-							}}
-						/>
-						<div className="d-flex flex-column justify-content-center">
-							<i
-								className="nc-icon nc-simple-add rounded-circle my-1 btn btn-primary p-0"
-								onClick={() => setQuantity(quantity + 1)}
-							/>
-							<i
-								className="nc-icon nc-simple-delete rounded-circle my-1 btn btn-primary p-0"
-								onClick={() => {
-									if (quantity > 1) {
-										setQuantity(quantity - 1);
-									}
-								}}
-							/>
-						</div>
-					</div>
-				</form>
-			</CardBody>
-		</Card>
-	);
+            <br />
+            {foundSupplierInfo.Website && (
+              <div>
+                <p>For more info visit their website:</p>
+                <a
+                  href={`https://${foundSupplierInfo.Website}`}
+                  target="_blank"
+                >
+                  {foundSupplierInfo.Website}
+                </a>
+              </div>
+            )}
+          </LongScrollingModal>
+          <br />
+          <strong>Container:</strong> {product.Container}
+          <br />
+          <strong>Quantity:</strong> {product.Quantity}
+          {product.Units}
+          <br />
+          <strong>Price:</strong> R{product.Price}
+          <br />
+        </CardText>
+        <form
+          className="d-flex justify-content-around"
+          onSubmit={addToCartClicked}
+        >
+          <Button color={btnColor} type="submit" disabled={btnDisabled}>
+            {btnText}
+            {btnText === "Added" && (
+              <i
+                className="nc-icon nc-check-2 ml-4"
+                style={{ fontSize: "15px" }}
+              />
+            )}
+          </Button>
+          <div className="d-flex justify-content-center">
+            <input
+              type="text"
+              value={quantity}
+              style={{ width: "30px" }}
+              className="m-3"
+              onChange={(e) => {
+                if (!Number(e.target.value) && e.target.value !== "") return;
+                setQuantity(Number(e.target.value));
+              }}
+            />
+            <div className="d-flex flex-column justify-content-center">
+              <i
+                className="nc-icon nc-simple-add rounded-circle my-1 btn btn-primary p-0"
+                onClick={() => setQuantity(quantity + 1)}
+              />
+              <i
+                className="nc-icon nc-simple-delete rounded-circle my-1 btn btn-primary p-0"
+                onClick={() => {
+                  if (quantity > 1) {
+                    setQuantity(quantity - 1);
+                  }
+                }}
+              />
+            </div>
+          </div>
+        </form>
+      </CardBody>
+    </Card>
+  );
 };
 
 const mapStateToProps = (state) => ({
-	supplierInfo: state.shop.supplierInfo,
+  supplierInfo: state.shop.supplierInfo,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-	addItem: (item, quantity) => dispatch(addItem(item, quantity)),
+  addItem: (item, quantity) => dispatch(addItem(item, quantity)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProductCard);
