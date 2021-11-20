c20320e4a4fe7261c014d9e9d242fef30b7ab33f
Jono Prest <jjprest@gmail.com>
2021-11-20 15:59:06 +0200

    changed sign in button to display get in touch if shop is not live

--- src/components/Headers/LandingPageHeader.js
+++ src/components/Headers/LandingPageHeader.js
@@ -27,78 +27,78 @@ import { Link } from "react-router-dom";
 // core components
 
 function LandingPageHeader({ currentUser, shopSettings, setLoginModal }) {
-	let pageHeader = React.createRef();
+  let pageHeader = React.createRef();
 
-	React.useEffect(() => {
-		if (window.innerWidth < 991) {
-			const updateScroll = () => {
-				let windowScrollTop = window.pageYOffset / 3;
-				pageHeader.current.style.transform =
-					"translate3d(0," + windowScrollTop + "px,0)";
-			};
-			window.addEventListener("scroll", updateScroll);
-			return function cleanup() {
-				window.removeEventListener("scroll", updateScroll);
-			};
-		}
-	});
+  React.useEffect(() => {
+    if (window.innerWidth < 991) {
+      const updateScroll = () => {
+        let windowScrollTop = window.pageYOffset / 3;
+        pageHeader.current.style.transform =
+          "translate3d(0," + windowScrollTop + "px,0)";
+      };
+      window.addEventListener("scroll", updateScroll);
+      return function cleanup() {
+        window.removeEventListener("scroll", updateScroll);
+      };
+    }
+  });
 
-	return (
-		<>
-			<div
-				style={{
-					backgroundImage:
-						"url(" + require("assets/img/splash-image.jpg") + ")",
-				}}
-				className="page-header"
-				data-parallax={true}
-				ref={pageHeader}
-			>
-				<div className="filter" />
-				<Container>
-					<div className="motto text-center">
-						<h1 style={{ letterSpacing: "5px" }}>Local Healthy Ethical</h1>
-						<h3>Buying good food made easy.</h3>
-						<br />
+  return (
+    <>
+      <div
+        style={{
+          backgroundImage:
+            "url(" + require("assets/img/splash-image.jpg") + ")",
+        }}
+        className="page-header"
+        data-parallax={true}
+        ref={pageHeader}
+      >
+        <div className="filter" />
+        <Container>
+          <div className="motto text-center">
+            <h1 style={{ letterSpacing: "5px" }}>Local Healthy Ethical</h1>
+            <h3>Buying good food made easy.</h3>
+            <br />
 
-						{shopSettings.shopIsLive && currentUser ? (
-							currentUser.userAccepted ? (
-								<Button
-									to="/shop"
-									tag={Link}
-									className="btn-round mr-1"
-									color="neutral"
-									outline
-								>
-									SHOP NOW
-								</Button>
-							) : (
-								<a href="#contact">
-									<Button className="btn-round mr-1" color="neutral" outline>
-										Get in touch
-									</Button>
-								</a>
-							)
-						) : (
-							<Button
-								onClick={() => setLoginModal(true)}
-								className="btn-round mr-1"
-								color="neutral"
-								outline
-							>
-								SIGN IN
-							</Button>
-						)}
-					</div>
-				</Container>
-			</div>
-		</>
-	);
+            {currentUser ? (
+              currentUser.userAccepted && shopSettings.shopIsLive ? (
+                <Button
+                  to="/shop"
+                  tag={Link}
+                  className="btn-round mr-1"
+                  color="neutral"
+                  outline
+                >
+                  SHOP NOW
+                </Button>
+              ) : (
+                <a href="#contact">
+                  <Button className="btn-round mr-1" color="neutral" outline>
+                    Get in touch
+                  </Button>
+                </a>
+              )
+            ) : (
+              <Button
+                onClick={() => setLoginModal(true)}
+                className="btn-round mr-1"
+                color="neutral"
+                outline
+              >
+                SIGN IN
+              </Button>
+            )}
+          </div>
+        </Container>
+      </div>
+    </>
+  );
 }
 
 const mapStateToProps = (state) => ({
-	currentUser: state.user.currentUser,
-	shopSettings: state.shop.shopSettings,
+  currentUser: state.user.currentUser,
+  shopSettings: state.shop.shopSettings,
 });
 
 export default connect(mapStateToProps)(LandingPageHeader);
